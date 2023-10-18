68abe2beaba6e833e4aa41d4dcf0e59f8363609b
YuriCJardim <yuricjardim@gmail.com>
2023-10-18 00:17:08 -0300

    test - testando funções da classe Pessoa

---
+++ src/test/java/com/correa/junit/PessoaTeste.java
@@ -0,0 +1,27 @@
+package com.correa.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class PessoaTeste {
+
+    @Test
+    void deveCalcularIdadeCorretamente() {
+
+        Pessoa jessica = new Pessoa("Jessica", LocalDateTime.of(2000, 1, 1, 15, 0));
+        Assertions.assertEquals(23, jessica.getIdade());
+    }
+
+    @Test
+    void deveRetornarSeEhMaiorDeIdade() {
+        Pessoa jessica = new Pessoa("Jessica", LocalDateTime.of(2000, 1, 1, 15, 0));
+        Assertions.assertTrue(jessica.ehMaiorDeIdade());
+
+
+        Pessoa joao = new Pessoa("João", LocalDateTime.now());
+        Assertions.assertFalse(joao.ehMaiorDeIdade());
+    }
+
+}
